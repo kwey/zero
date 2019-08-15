af5b17f347b93b6288af811cbbd041311788a717
kwe <zhangjq92@163.com>
2019-08-15 19:17:02 +0800

    refactor: frame

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 /// <reference path='../types/index.d.ts'/>
 
-import './static/index.less';
+import './static/index.less'
 
 import Main from './ts/main'
 import { metadata, IData } from './metadata'
 
 export interface IConfig {
-    container: HTMLElement
+    container: string
     name: string
     prefix?: string
     metadata?: IData
 }
-// 此模块建议只做模块的初始化，不做任何逻辑
+
 export default class KWE {
     main: Main
     config: IConfig
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,60 +1,17 @@
 import { IConfig } from '..'
-import Utils from './utils'
-import searchSvg from '../static/img/search.svg'
+import Search from './search'
 
-interface ITemplete {
-    input: HTMLInputElement
-    btn: HTMLButtonElement
-    list: HTMLUListElement
-}
+// 此模块建议只做模块的初始化，不做任何逻辑
 export default class Main {
-    prefix: string
+    search: Search
     config: IConfig
-    container: HTMLElement
-    templete: ITemplete
 
     constructor(config: IConfig) {
         this.config = config
-        this.prefix = config.prefix
-        this.container = config.container
-        this.container.classList.add(this.config.prefix)
         this.init()
-        this.events()
     }
 
     init() {
-        this.container.innerHTML = this.tpl()
-        this.templete = {
-            input: this.container.querySelector(`.${this.prefix}-text`),
-            btn: this.container.querySelector(`.${this.prefix}-btn`),
-            list: this.container.querySelector(`.${this.prefix}-list`)
-        }
-    }
-
-    private events() {
-        this.templete.btn.addEventListener('click', () => {
-            this.appendList()
-        })
-
-        this.templete.input.addEventListener('keyup', (e: KeyboardEvent) => {
-            if (e.keyCode === 13) {
-                this.appendList()
-            }
-        })
-    }
-
-    private appendList() {
-        const li = Utils.parseDom(`<li>${Utils.htmlEscape(this.templete.input.value)}</li>`)
-        this.templete.list.appendChild(li)
-        this.templete.input.value = ''
-    }
-
-    private tpl() {
-        return `<h1 class="${this.prefix}-header">TYPESCRIPT DEMO</h1>
-            <div class="${this.prefix}-search">
-                <input type="text" class="${this.prefix}-text" placeholder="search">
-                <span class="${this.prefix}-btn">${searchSvg}</span>
-            </div>
-            <ul class="${this.prefix}-list"></ul>`
+        this.search = new Search(this.config)
     }
 }
